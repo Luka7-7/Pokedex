eee29f6b23058d0875cbe69359ad1c1bba93fe8e
Luka7-7 <teseliamaster@outlook.com>
2026-07-30 18:37:44 -0700

    fix: redirigir raiz a tipo por defecto

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,10 @@
 import { useState, useEffect } from 'react'
-import reactLogo from './assets/react.svg'
-import { Routes, Route, useParams, useNavigate } from 'react-router-dom'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
+import { Routes, Route, useParams, useNavigate, Navigate } from 'react-router-dom'
 import TypeNav from './components/TypeNav'
 import PokemonList from './components/PokemonList'
 import './App.css'
 
-function TypePage({types}) {
+function TypePage({ types }) {
   const { typeName } = useParams()
   const navigate = useNavigate()
   const [limit, setLimit] = useState(20)
@@ -15,37 +12,47 @@ function TypePage({types}) {
   useEffect(() => {
     setLimit(20)
   }, [typeName])
-      return (
-      <div className='min-h-screen bg-gray-100 pb-24' >
-        <header className='bg-black text-white text-center py-6 shadow-md'>
-          <h1 className='text-3xl font-bold tracking-wide' >Pokedex</h1>
-          <p className='capitalize text-shadow-blue-500 mt-1'>Type: {typeName}</p>
-        </header>
-        <PokemonList selectedType={typeName} limit={limit} onLoadMore={()=>setLimit((prev)=>prev+20)} />
-        <TypeNav types={types} selectedType={typeName} onSelectType={(type)=>navigate(`/tipo/${type}`)} />
 
-      </div>
-    )
+  return (
+    <div className='min-h-screen bg-gray-100 pb-24'>
+      <header className='bg-black text-white text-center py-6 shadow-md'>
+        <h1 className='text-3xl font-bold tracking-wide'>Pokedex</h1>
+        <p className='capitalize text-blue-300 mt-1'>Type: {typeName}</p>
+      </header>
+      <PokemonList
+        selectedType={typeName}
+        limit={limit}
+        onLoadMore={() => setLimit((prev) => prev + 20)}
+      />
+      <TypeNav
+        types={types}
+        selectedType={typeName}
+        onSelectType={(type) => navigate(`/tipo/${type}`)}
+      />
+    </div>
+  )
 }
 
 function App() {
   const [types, setTypes] = useState([])
+
   useEffect(() => {
     fetch('https://pokeapi.co/api/v2/type')
       .then((res) => res.json())
       .then((data) => {
-        const tiposValidos = data.results.filter((t)=>t.name !=='unknown' && t.name !== 'shadow')
+        const tiposValidos = data.results.filter(
+          (t) => t.name !== 'unknown' && t.name !== 'shadow'
+        )
         setTypes(tiposValidos)
       })
-  },[])
+  }, [])
 
   return (
     <Routes>
-      <Route path="/" element={<TypePage types={types} />} />
+      <Route path="/" element={<Navigate to="/tipo/fire" replace />} />
       <Route path="/tipo/:typeName" element={<TypePage types={types} />} />
     </Routes>
   )
-
 }
 
 export default App
